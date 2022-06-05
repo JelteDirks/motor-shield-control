656318c9c34a3741a83595340385f00169432d1f
Jelte Dirks <j.dirks@outlook.com>
2022-06-05 22:02:28 +0200

    remove motor update in a test
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -6,7 +6,6 @@ fn pre_setting_values() {
     let mut board: AMSBoard = AMSBoard::new(BoardType::BCM);
     let mut m1: Motor = Motor::new();
     m1.set_direction(Direction::Counterclockwise);
-    m1.set_pin(16);
     board.set_motor(m1, 1);
     assert_eq!(8, board.get_directions());
 }
