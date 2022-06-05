22cda299adc0f7feb38e464e619ee3d974c9a462
Jelte Dirks <j.dirks@outlook.com>
2022-06-05 17:42:53 +0200

    implement test for board directions after adding one motor
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,6 +1,12 @@
-use motor_shield_control::*; 
+use motor_shield_control::board::{AMSBoard, BoardType};
+use motor_shield_control::motor::{Motor, Direction};
 
 #[test]
 fn run_a_motor() {
-    assert_eq!(2, 1);
+    let mut board: AMSBoard = AMSBoard::new(BoardType::BCM);
+    let mut m1: Motor = Motor::new();
+    m1.set_direction(Direction::Counterclockwise);
+    m1.set_pin(16);
+    board.set_motor(m1, 1);
+    assert_eq!(4, board.get_directions());
 }
