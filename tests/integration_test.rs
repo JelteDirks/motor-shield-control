442b270a7b3d16981dc8618ba88f83263bf351ca
Jelte Dirks <j.dirks@outlook.com>
2022-06-05 19:26:58 +0200

    test setting of motor values before adding to board
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -9,4 +9,7 @@ fn run_a_motor() {
     m1.set_pin(16);
     board.set_motor(m1, 1);
     assert_eq!(8, board.get_directions());
+
+    board.change_motor_direction(1, Direction::Clockwise);
+    assert_eq!(4, board.get_directions());
 }
