07c288a843e6ee4262366240f6084051611e5af5
Jelte Dirks <j.dirks@outlook.com>
2022-06-06 20:58:19 +0200

    separate initialization to helper methods, postfix with type of initialization
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- src/servo.rs
+++ src/servo.rs
@@ -7,7 +7,11 @@ pub struct Servo {
 }
 
 impl Servo {
-    pub fn new(pin: u8) -> Servo  {
+    pub fn new_default(pin: u8) -> Servo  {
+        return Servo::new_servo_from_config(ServoConfig::new_default(), pin);
+    }
+
+    pub fn new_servo_from_config(cfg: ServoConfig, pin: u8) -> Servo {
         let gpio = match Gpio::new() {
             Ok(g) => g,
             Err(e) => panic!("{:?}", e),
@@ -16,21 +20,14 @@ impl Servo {
         let gpio_pin = match gpio.get(pin) {
             Ok(gp) => gp,
             Err(e) => panic!("{:?}", e),
-        };
-
-        const cycle: Duration = Duration::from_millis(20);
-        const width: Duration = Duration::from_micros(1500);
+        }; 
 
         let mut output_pin = gpio_pin.into_output_low();
-        output_pin.set_pwm(cycle, width);
+        output_pin.set_pwm(cfg.cycle, cfg.width);
 
         return Servo {
             pin: Some(output_pin),
-            config: ServoConfig {
-                cycle: Duration::from_millis(20),
-                width: Duration::from_micros(1500),
-                angle: 90,
-            }
+            config: cfg
         }
     }
 
@@ -86,11 +83,19 @@ impl ServoConfig {
         }
     }
 
-    pub fn new_pwm(w: Duration) -> ServoConfig {
+    pub fn new_default() -> ServoConfig {
+        return ServoConfig {
+            cycle: Duration::from_millis(20),
+            width: Duration::from_micros(1500),
+            angle: 90,
+        }
+    }
+
+    pub fn new_config_from_width(w: Duration) -> ServoConfig {
         return ServoConfig::new(w);
     }
 
-    pub fn new_angle(a: u16) -> ServoConfig {
+    pub fn new_config_from_angle(a: u16) -> ServoConfig {
         let width = ServoConfig::calc_width_from_angle(a);
         return ServoConfig::new(width);
     }
@@ -119,31 +124,31 @@ mod tests {
 
     #[test]
     fn test_max_angle_calc() {
-        let cfg = ServoConfig::new_angle(180);
+        let cfg = ServoConfig::new_config_from_angle(180);
         assert_eq!(cfg.width, Duration::from_micros(2000)); 
     }
 
     #[test]
     fn test_90_angle_calc() {
-        let cfg = ServoConfig::new_angle(90);
+        let cfg = ServoConfig::new_config_from_angle(90);
         assert_eq!(cfg.width, Duration::from_micros(1500)); 
     }
 
     #[test]
     fn test_min_angle_calc() {
-        let cfg = ServoConfig::new_angle(0);
+        let cfg = ServoConfig::new_config_from_angle(0);
         assert_eq!(cfg.width, Duration::from_micros(1000));
     }
 
     #[test]
     fn test_arbirary_angles() {
-        let cfg = ServoConfig::new_angle(52);
+        let cfg = ServoConfig::new_config_from_angle(52);
         assert_eq!(cfg.width, Duration::from_micros(1288));
     }
 
     #[test]
     fn test_arbirary_angles2() {
-        let cfg = ServoConfig::new_angle(134);
+        let cfg = ServoConfig::new_config_from_angle(134);
         assert_eq!(cfg.width, Duration::from_micros(1744));
     }
 }
