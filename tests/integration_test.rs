364dfeed2460d5ab01238b38c0f2395a0dd8b6e2
Jelte Dirks <j.dirks@outlook.com>
2022-06-06 17:51:48 +0200

    add some tests for setting pins, only for on rpi
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,5 +1,6 @@
 use motor_shield_control::board::{AMSBoard, BoardType};
-use motor_shield_control::motor::{Motor, Direction};
+use motor_shield_control::motor::{Motor, Direction, Status};
+use std::matches;
 
 #[test]
 fn pre_setting_values() {
@@ -21,4 +22,48 @@ fn post_setting_values() {
     let mut m2: Motor = Motor::new();
     board.set_motor(m2, 2);
     assert_eq!(10, board.get_directions());
+}
+
+#[test]
+fn test_motor_changed_direction() {
+    let mut board = AMSBoard::new(BoardType::BCM);
+    let mut motor = Motor::new();
+    board.set_motor(motor, 1);
+    board.get_motor(1).as_mut().unwrap().set_direction(Direction::Counterclockwise);
+    board.start_motor_full(1);
+    assert!(matches!(board.get_motor(1).as_mut().unwrap().get_direction(), Direction::Counterclockwise));
+}
+
+#[test]
+fn test_motor_default_direction() {
+    let mut board = AMSBoard::new(BoardType::BCM);
+    let mut motor = Motor::new();
+    board.set_motor(motor, 1);
+    board.start_motor_full(1);
+    assert!(matches!(board.get_motor(1).as_mut().unwrap().get_direction(), Direction::Clockwise));
+}
+
+#[test]
+fn test_motor_pin_setting() {
+    let mut motor = Motor::new();
+    motor.set_pin(16);
+
+}
+
+#[test]
+fn test_motor_status() {
+    let mut board = AMSBoard::new(BoardType::BCM);
+    let mut motor = Motor::new();
+    motor.set_pin(16);
+    board.set_motor(motor, 1);
+    board.start_motor_full(1);
+    let status = board.get_motor(1).as_mut().unwrap().get_status();
+    assert!(matches!(board.get_motor(1).as_mut().unwrap().get_status(), Status::Running));
+}
+
+#[test]
+fn test_unset_motor() {
+    let mut board = AMSBoard::new(BoardType::BCM);
+    let result =  board.start_motor_full(1); 
+    assert!(result.is_err());
 }
