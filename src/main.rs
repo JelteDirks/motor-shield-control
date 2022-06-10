4a854994070d4959299ed75807ee727292ebae4e
Jelte <j.dirks@outlook.com>
2022-06-10 15:33:49 +0200

    test board using range

--- src/main.rs
+++ src/main.rs
@@ -20,12 +20,16 @@ fn start_motor(n: usize) {
     let mut board = AMSBoard::new(BoardType::BCM);
     let mut motor = Motor::new();
 
+    let cycle = Duration::from_millis(10);
+    let up = Duration::from_millis(20);
+    let low = Duration::from_millis(2);
+    let step = Duration::from_millis(2);
+
     motor.set_pin(21);
     board.set_shift_register_pins(16, 20, 19);
 
     board.set_motor(motor, n);
-    board.start_motor_full(n);
-
+    board.test_motor_range(n, cycle, low, up, step);
     sleep(Duration::from_secs(2));
 
 }
