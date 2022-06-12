769368e002d8b02e97abcddd24098ada64f32746
Jelte Dirks <j.dirks@outlook.com>
2022-06-12 13:14:00 +0200

    move pulse cycle const to the config struct, add documentation
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- src/servo.rs
+++ src/servo.rs
@@ -2,18 +2,23 @@ use rppal::gpio::{OutputPin, Gpio};
 use std::time::Duration;
 use std::thread::sleep;
 
+/// The servo struct. This is specifically written for the Tower Pro SG90.
 pub struct Servo {
+    /// The pin that is used to address the servo.
     pin: Option<OutputPin>,
+    /// The configuration that the servo is currently operating in.
     config: ServoConfig,
 }
 
 impl Servo {
-    const PULSE_CYCLE: Duration = Duration::from_millis(20); 
 
+    /// Returns a new servo from scratch. Only the pin number is required to
+    /// create a new servo.
     pub fn new_default(pin: u8) -> Servo  {
         return Servo::new_servo_from_config(ServoConfig::new_default(), pin);
     }
 
+    /// Returns a new servo from a configuration. A pin should also be provided.
     pub fn new_servo_from_config(cfg: ServoConfig, pin: u8) -> Servo {
         let gpio = match Gpio::new() {
             Ok(g) => g,
@@ -34,10 +39,15 @@ impl Servo {
         }
     }
 
+    /// Rteurns a reference to the configuration that this servo is currently 
+    /// using to operate.
     pub fn get_config(&self) -> &ServoConfig {
         return &self.config;
     }
 
+    /// Sets the angle of this servo to the specified degress. The static
+    /// method `calc_width_from_angle()` on `ServoConfig` is used to calculate
+    /// the pulse width that is needed to reach the angle.
     pub fn set_angle(&mut self, a: u16) -> Result<(), ServoError> {
         let width = ServoConfig::calc_width_from_angle(a);
 
@@ -59,14 +69,18 @@ impl Servo {
         return Ok(());
     }
 
+    /// Returns the duration of the pulse width of this servo.
     pub fn get_pulse_width(self) -> Duration {
         return self.config.width;
     }
 
+    /// Returns the angle that this servo is currently in.
     pub fn get_angle(self) -> u16 {
         return self.config.angle;
     }
 
+    /// Tests the servo range using a lower and upper bound on the pulse width.
+    /// A pin should also be specified.
     pub fn test_range(pin: u8, low: Duration, up: Duration) {
         let gpio = match Gpio::new() {
             Ok(g) => g,
@@ -80,7 +94,7 @@ impl Servo {
 
         let mut output_pin = gpio_pin.into_output_low();
         let mut cur = low;
-        let cycle = Servo::PULSE_CYCLE;
+        let cycle = ServoConfig::PULSE_CYCLE;
         while cur <= up {
             println!("testing servo range width={:?} and cycle{:?}", cur, cycle);
             sleep(Duration::from_millis(500));
@@ -103,10 +117,18 @@ pub struct ServoConfig {
 }
 
 impl ServoConfig {
+    /// The minimum pulse width of the servo, used to calculate angles.
     const MIN: f32 = 600.0; 
+    /// The maximum pulse width of the servo, used to calculate angles.
     const MAX: f32 = 2550.0;
+    /// The maximum angle that the servo supports.
     const MAX_ANGLE: u16 = 180; 
+    /// The pulse cycle of the servo. This is made for servos that use a 
+    /// frequency of 50 Herz or 20 milliseconds.
+    const PULSE_CYCLE: Duration = Duration::from_millis(20); 
 
+    /// Returns a new configuration with a specified pulse width. The cycle
+    /// is set to the 50 hz default cycle length.
     fn new(width: Duration) -> ServoConfig {
         if width < Duration::from_micros(ServoConfig::MIN as u64) {
             panic!("pulse width should be at least 0.4ms");
@@ -117,17 +139,18 @@ impl ServoConfig {
         }
 
         return ServoConfig {
-            cycle: Duration::from_millis(20),
+            cycle: ServoConfig::PULSE_CYCLE,
             width: width,
             angle: 0
         }
     }
 
     pub fn new_default() -> ServoConfig {
+        const default_angle: u16 = 90;
         return ServoConfig {
-            cycle: Duration::from_millis(20),
-            width: Duration::from_micros(1500),
-            angle: 90,
+            cycle: ServoConfig::PULSE_CYCLE,
+            width: ServoConfig::calc_width_from_angle(default_angle),
+            angle: default_angle,
         }
     }
 
